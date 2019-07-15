04ca1a6499d6aed3cee84dc2a72b0b31bdecf013
rushi444 <rushi44@gmail.com>
2019-07-15 13:00:17 -0700

    Creates Country Class

---
+++ src/main/java/com/project/countries/Country.java
@@ -0,0 +1,78 @@
+package com.project.countries;
+
+public class Country {
+    private static int maxID = 0;
+    private int id;
+    private String name;
+    private int population;
+    private int landMassSize;
+    private int medianAge;
+
+    public Country(int id, String name, int population, int landMassSize, int medianAge) {
+        maxID++;
+        this.id = maxID;
+        this.name = name;
+        this.population = population;
+        this.landMassSize = landMassSize;
+        this.medianAge = medianAge;
+    }
+
+    public Country(Country toClone) {
+        this.id = toClone.getId();
+        this.name = toClone.getName();
+        this.population = toClone.getPopulation();
+        this.landMassSize = toClone.getLandMassSize();
+        this.medianAge = toClone.getMedianAge();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public int getLandMassSize() {
+        return landMassSize;
+    }
+
+    public void setLandMassSize(int landMassSize) {
+        this.landMassSize = landMassSize;
+    }
+
+    public int getMedianAge() {
+        return medianAge;
+    }
+
+    public void setMedianAge(int medianAge) {
+        this.medianAge = medianAge;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", population=" + population +
+                ", landMassSize=" + landMassSize +
+                ", medianAge=" + medianAge +
+                '}';
+    }
+}
